c1e73c0c193674a28c81f790635def4eb7ac4155
alecarraro <a.carraro@student.tudelft.nl>
2025-05-27 12:32:28 +0200

    edit function for laplacians

--- utils/operators.py
+++ utils/operators.py
@@ -1,52 +1,64 @@
-import torch
-import scipy as sp
-import networkx as nx
 import numpy as np
+
+def laplacian_standard(adj):
+    degrees = np.sum(adj, axis=1)
+    D = np.diag(degrees)
+    return D - adj
+
+def laplacian_hubs_repelling(adj):
+    degrees = np.sum(adj, axis=1)
+    safe_degrees = np.where(degrees == 0, 1, degrees)
+    
+    scaling = (degrees[:, None] / safe_degrees[None, :]) * (adj > 0)
+    diag_entries = np.sum(scaling, axis=1)
+    
+    Eps_A = np.diag(diag_entries)
+    D = np.diag(degrees)
+    D_inv = np.diag(1 / safe_degrees)
+    
+    return Eps_A - D @ adj @ D_inv
+
+def laplacian_hubs_attracting(adj):
+    degrees = np.sum(adj, axis=1)
+    safe_degrees = np.where(degrees == 0, 1, degrees)
+
+    scaling = (safe_degrees[None, :] / degrees[:, None]) * (adj > 0)
+    diag_entries = np.sum(scaling, axis=1)
+
+    Eps_R = np.diag(diag_entries)
+    D = np.diag(degrees)
+    D_inv = np.diag(1 / safe_degrees)
+
+    return Eps_R - D_inv @ adj @ D
 
 def compute_laplacian(adj, alpha=0):
     """
-    Get the Laplacian/normalized Laplacian matrix
-    --------------
-        adj: np.array
-            Symmetric adjacency matrix
-            Shape: (N, N)
-        
-        alpha: int
-            0: Laplacian
-            1: Hubs-repelling Laplacian
-            -1: Hubs-attracting Laplacian
-        
-    Returns
-    -------------
-        L: np.array
-            Resulting Laplacian matrix
+    Dispatch to one of the Laplacian variants based on alpha.
     """
-
-    # degree vector
-    degrees = np.sum(adj, axis=1)
-    D = np.diag(degrees)
-
-    # Compute the inverse of the degree matrix
-    D_inv = np.linalg.inv(D)
-
-    diag_entries_A = np.zeros(len(degrees))
-    diag_entries_R = np.zeros(len(degrees))
-
-    for i in range(len(degrees)):
-        neighbors = np.where(adj[i] > 0)[0]
-        diag_entries_A[i] = np.sum(degrees[i]/degrees[neighbors])
-        diag_entries_R[i] = np.sum(degrees[neighbors]/degrees[i])
-
-    # Create diagonal matrix
-    Eps_A = np.diag(diag_entries_A)
-    Eps_R = np.diag(diag_entries_R)
-
-    # Compute the Laplacian matrix
     if alpha == 0:
-        L = D - adj
+        return laplacian_standard(adj)
     elif alpha == 1:
-        L = Eps_A - D @ adj @ D_inv
+        return laplacian_hubs_repelling(adj)
+    elif alpha == -1:
+        return laplacian_hubs_attracting(adj)
     else:
-        L = Eps_R - D_inv @ adj @ D
-    return L
-    
+        raise ValueError("alpha must be -1, 0, or 1")
+
+#adj = np.array([[0, 1, 0, 0],[1, 0, 1, 0],[0, 1, 0, 1],[0, 0, 1, 0]])
+adj = np.array([[0, 1, 1, 1],
+                [1, 0, 1, 0],
+                [1, 1, 0, 0],
+                [1, 0, 0, 0]])              
+
+L_R = compute_laplacian(adj,1)
+L_A = compute_laplacian(adj,-1)
+L = compute_laplacian(adj,0)
+
+print("Hubs-attracting Laplacian matrix L_A:")
+print(L_A)
+
+print("Laplacian matrix L")
+print(L)
+
+print("Hubs-repelling Laplacian matrix L_R:")
+print(L_R)
