0513467ddd49711d8aec45526ad47c61940990f4
alecarraro <a.carraro@student.tudelft.nl>
2025-06-11 15:16:14 +0200

    added to adj

--- GCNN/gcnn.py
+++ GCNN/gcnn.py
@@ -1,8 +1,9 @@
 from filters import create_filter_list
-import torch 
+from torch_geometric.utils import to_dense_adj 
 import torch.nn as nn
 from conv_layer import ConvLayer
 import os
+import torch 
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'GAD', 'src')))
@@ -30,13 +31,26 @@ class GeneralPolyGNN(nn.Module):
         else:
             self.readout = None
 
-    def forward(self, X, batch, A):
+    def forward(self, X, batch, edge_index):
         """
         base_operator: the initial graph structure (could be Laplacian, adjacency, etc.)
         """
-        S = self.gso_generator(A)  # Generate current graph shift operator (differentiable)
+        adj = to_dense_adj(edge_index, batch)
+        gsos = []
+        num_nodes_per_graph = []
 
+        for i in range(adj.size(0)):
+            A_i = adj[i]
+            num_nodes_i = (A_i.sum(dim=1) != 0).sum().item()
+            num_nodes_per_graph.append(num_nodes_i)
+            A_i = A_i[:num_nodes_i, :num_nodes_i]  # crop to real adj since torch pads
+            S_i = self.gso_generator(A_i) 
+            gsos.append(S_i)
+        
+        # sparse block diagonal matrix
+        S = torch.block_diag(*gsos)
         x = X
+
         for conv_layer, degree, activation in self.layers:
             filter_list = create_filter_list(S, degree)  # Create polynomial filters based on current S
             conv_layer.filters = filter_list  # Dynamically set filters
@@ -49,3 +63,7 @@ class GeneralPolyGNN(nn.Module):
         if self.readout is not None:
             return self.readout(x).squeeze(-1)
         return x
+    
+    def pooling(self, x, batch):
+        from torch_geometric.nn import global_mean_pool
+        return global_mean_pool(x, batch)
